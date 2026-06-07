c1cfdf9643e5180f4d204d2ec263362cb68b4338
Agencia Krea <agenciakrea.contacto@gmail.com>
2026-06-07 18:19:00 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    ▸ SUBÍ ESTE NÚMERO CADA VEZ QUE ACTUALICES LA APP  ◂
    (debe coincidir conceptualmente con APP_VERSION del index.html)
    ═══════════════════════════════════════════════════════════════ */
-const VERSION = 'v12';
+const VERSION = 'v13';
 const CACHE = 'silva-fatiga-' + VERSION;
 
 const ASSETS = [
